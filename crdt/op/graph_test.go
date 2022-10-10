6afd7855feba824b4cc99bd773ce772adc8dda58
Rafael Chaves <rafaelvchaves@gmail.com>
2022-10-10 16:24:32 -0400

    Add query helper function in op graph tests

--- crdt/op/graph_test.go
+++ crdt/op/graph_test.go
@@ -4,31 +4,23 @@ import (
 	"testing"
 )
 
+func queryT(t *testing.T, g CmRDT[Graph], cmd string, arg string, want string) {
+	got, err := g.Query(cmd, arg)
+	if err != nil {
+		t.Fatalf("unexpected Query() error: %v", err)
+	}
+	if got != want {
+		t.Errorf("Query(%s %s): got = %q, want %q", cmd, arg, got, want)
+	}
+}
+
 func TestAddVertex(t *testing.T) {
 	g := Init(NewGraph(), graphHandlers, graphQueries)
 	g.Process(AddVertexCmd, "a")
 	g.Process(AddVertexCmd, "b")
-	exists, err := g.Query(ExistsVertexCmd, "a")
-	if err != nil {
-		t.Fatalf("unexpected Query() error: %q", err)
-	}
-	if exists != "true" {
-		t.Errorf("Query(EXISTSV a): expected true, got %s", exists)
-	}
-	exists, err = g.Query(ExistsVertexCmd, "b")
-	if err != nil {
-		t.Fatalf("unexpected Query() error: %q", err)
-	}
-	if exists != "true" {
-		t.Errorf("Query(EXISTSV b): expected true, got %s", exists)
-	}
-	neighbors, err := g.Query(NeighborsCmd, "a")
-	if err != nil {
-		t.Fatalf("unexpected Query() error: %q", err)
-	}
-	if neighbors != "{}" {
-		t.Errorf("Query(NEIGH a): expected {}, got %q", neighbors)
-	}
+	queryT(t, g, ExistsVertexCmd, "a", "true")
+	queryT(t, g, ExistsVertexCmd, "b", "false")
+	queryT(t, g, NeighborsCmd, "a", "{}")
 }
 
 func TestAddEdge(t *testing.T) {
@@ -62,23 +54,11 @@ func TestRemove(t *testing.T) {
 	g.Process(AddEdgeCmd, NewEdge("a", "b"))
 	g.Process(AddEdgeCmd, NewEdge("a", "c"))
 	g.Process(RemoveVertexCmd, "a")
-	exists, err := g.Query(ExistsVertexCmd, "a")
-	if err != nil {
-		t.Fatalf("unexpected Query() error: %q", err)
-	}
-	if exists != "true" {
-		t.Errorf("Query(EXISTSV a), expected true, got false")
-	}
+	queryT(t, g, ExistsVertexCmd, "a", "true")
 	g.Process(RemoveEdgeCmd, NewEdge("a", "b"))
 	g.Process(RemoveEdgeCmd, NewEdge("a", "c"))
 	g.Process(RemoveVertexCmd, "a")
-	exists, err = g.Query(ExistsVertexCmd, "a")
-	if err != nil {
-		t.Fatalf("unexpected Query() error: %q", err)
-	}
-	if exists != "false" {
-		t.Errorf("Query(EXISTSV a), expected false, got true")
-	}
+	queryT(t, g, ExistsVertexCmd, "a", "false")
 }
 
 
