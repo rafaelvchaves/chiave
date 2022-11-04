73758e778ad1b6719629695d2e4a13903fe65c3d
Rafael Chaves <rafaelvchaves@gmail.com>
2022-11-04 15:38:39 -0400

    Simplify state-based set implementation

--- crdt/state/set.go
+++ crdt/state/set.go
@@ -1,114 +1,112 @@
 package state
 
 import (
+	"kvs/crdt"
 	pb "kvs/proto"
 	"kvs/util"
 )
 
-type metadata struct {
-	timestamp int64
-	replica   string
-}
-
 type Set struct {
 	replica util.Replica
-	set     map[string]util.Set[metadata]
-	vclock  GCounter
+	add     map[string]GCounter
+	rem     map[string]GCounter
 }
+
+var _ crdt.Set = &Set{}
 
 func NewSet(replica util.Replica) *Set {
 	return &Set{
 		replica: replica,
-		set:     make(map[string]util.Set[metadata]),
-		vclock:  NewGCounter(replica),
+		add:     make(map[string]GCounter),
+		rem:     make(map[string]GCounter),
 	}
 }
 
 func (s *Set) Lookup(e string) bool {
-	_, ok := s.set[e]
-	return ok
+	// _, ok := s.set[e]
+	return true
+}
+
+func (s *Set) Value() map[string]struct{} {
+	set := make(map[string]struct{})
+	for k := range s.add {
+		set[k] = struct{}{}
+	}
+	for k, vr := range s.rem {
+		// if the element exists in the remove set
+		// with a strictly greater timestamp,
+		// remove it from the resulting set.
+		if va, ok := s.add[k]; ok && va.Compare(vr) == LT {
+			delete(set, k)
+		}
+	}
+	return set
 }
 
 func (s *Set) Add(e string) {
-	c := s.vclock.Value() + 1
-	s.vclock.Increment()
-	md := metadata{
-		timestamp: int64(c),
-		replica:   s.replica.String(),
-	}
-	mds, ok := s.set[e]
-	if !ok {
-		s.set[e] = util.NewSet(md)
+	if va, ok := s.add[e]; ok {
+		va.Increment()
+		delete(s.rem, e)
 		return
 	}
-	mds.RemoveWhere(func(md metadata) bool {
-		return md.timestamp < int64(c) // remove all entries older than new timestamp
-	})
-	mds.Add(md)
+	if vr, ok := s.rem[e]; ok {
+		s.add[e] = vr
+		vr.Increment()
+		delete(s.rem, e)
+		return
+	}
+	vec := NewGCounter(s.replica)
+	vec.Increment()
+	s.add[e] = vec
 }
 
 func (s *Set) Remove(e string) {
-	delete(s.set, e)
-}
-
-func union(a, b map[string]util.Set[metadata]) map[string]util.Set[metadata] {
-	m := make(map[string]util.Set[metadata])
-	for e, mdsa := range a {
-		mdsb, ok := b[e]
-		if !ok {
-			m[e] = mdsa
-			continue
-		}
-		m[e] = util.Union(mdsa, mdsb)
-	}
-	for e, mdsb := range b {
-		if _, ok := m[e]; !ok {
-			m[e] = mdsb
-		}
-	}
-	return m
-}
-
-func addSafe(m map[string]util.Set[metadata], e string, md metadata) {
-	old, ok := m[e]
-	if !ok {
-		m[e] = util.NewSet(md)
+	if va, ok := s.add[e]; ok {
+		s.rem[e] = va
+		va.Increment()
+		delete(s.add, e)
 		return
 	}
-	old.Add(md)
+	if vr, ok := s.rem[e]; ok {
+		s.add[e] = vr
+		vr.Increment()
+		delete(s.add, e)
+		return
+	}
+	vec := NewGCounter(s.replica)
+	vec.Increment()
+	s.rem[e] = vec
 }
 
-func (s *Set) Merge(o Set) {
-	U := make(map[string]util.Set[metadata])
-	for e, ma := range s.set {
-		ma.ForEach(func(m metadata) {
-			mb, ok := o.set[e]
-			if (ok && mb.Contains(m)) || m.timestamp > o.vclock.SafeGet(m.replica) {
-				// means that (e, timestamp, replica) is in union(M, M')
-				addSafe(U, e, m)
-			}
-		})
+func (s *Set) Merge(add, rem map[string]map[string]int64) {
+	for k, v := range add {
+		vo, ok := s.add[k]
+		if ok {
+			vo.Merge(OfMap(v))
+			continue
+		}
+		s.add[k] = OfMap(v)
 	}
-
-	for e, mb := range o.set {
-		mb.ForEach(func(m metadata) {
-			ma, ok := s.set[e]
-			if (!ok || !ma.Contains(m)) && m.timestamp > s.vclock.SafeGet(m.replica) {
-				// means that (e, timestamp, replica) is in M''
-				addSafe(U, e, m)
-			}
-		})
+	for k, v := range rem {
+		vo, ok := s.rem[k]
+		if ok {
+			vo.Merge(OfMap(v))
+			continue
+		}
+		s.rem[k] = OfMap(v)
 	}
-
-	for _, mu := range U {
-		mu.RemoveWhere(func(m1 metadata) bool {
-			return mu.Exists(func(m2 metadata) bool {
-				return m1.replica == m2.replica && m2.timestamp > m1.timestamp
-			})
-		})
+	for k, vr := range s.rem {
+		va, ok := s.add[k]
+		if ok && vr.Compare(va) == GT {
+			delete(s.add, k)
+		}
 	}
-	s.set = U
-	s.vclock.Merge(o.vclock.vec)
+	for k, va := range s.add {
+		vr, ok := s.add[k]
+		if ok && va.Compare(vr) == GT {
+			delete(s.rem, k)
+		}
+	}
 }
 
 func (s *Set) GetEvent() *pb.Event {
