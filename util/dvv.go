4ba5d1d08c667917b375c47e3d51669df72e5393
Rafael Chaves <rafaelvchaves@gmail.com>
2022-11-05 22:04:00 -0400

    Add reconciliation method for DVVs

--- util/dvv.go
+++ util/dvv.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	pb "kvs/proto"
 
 	"golang.org/x/exp/constraints"
@@ -11,9 +12,69 @@ type dot struct {
 	n int32
 }
 
-type DVV struct {
-	d  dot
-	vv map[string]int32
+type OpType int
+
+type Operation struct {
+	OpType OpType
+	args   any
+}
+
+func displayMap(m map[string]int32) string {
+	str := "{"
+	i := 0
+	for k, v := range m {
+		i++
+		str += fmt.Sprintf("%s:%d", k, v)
+		if i < len(m) {
+			str += ","
+		}
+	}
+	return str + "}"
+}
+
+func String(context ...*pb.DVV) string {
+	str := ""
+	for _, dvv := range context {
+		str += fmt.Sprintf("(%s:%d, %s)", dvv.Dot.Replica, dvv.Dot.N, displayMap(dvv.Clock))
+	}
+	return str
+}
+
+func dvvVals(dvv *pb.DVV) []string {
+	var result []string
+	result = append(result, dvv.Sibling)
+	for r := range dvv.Clock {
+		result = append(result, r)
+	}
+	return result
+}
+
+func values(D []*pb.DVV) []string {
+	var result []string
+	for _, dvv := range D {
+		result = append(result, dvvVals(dvv)...)
+	}
+	return result
+}
+
+func Reconcile(f func([]string) string, D []*pb.DVV, r string) *pb.DVV {
+	d := Join(D)
+	d.Sibling = f(values(D))
+	d.Dot = &pb.Dot{
+		Replica: r,
+		N:       ceil(D, r),
+	}
+	return d
+}
+
+func Join(D []*pb.DVV) *pb.DVV {
+	result := &pb.DVV{
+		Clock: make(map[string]int32),
+	}
+	for _, i := range ids(D) {
+		result.Clock[i] = ceil(D, i)
+	}
+	return result
 }
 
 func Lt(d1, d2 *pb.DVV) bool {
@@ -87,7 +148,7 @@ func Update(S []*pb.DVV, S_r []*pb.DVV, r string) *pb.DVV {
 	result := &pb.DVV{
 		Dot: &pb.Dot{
 			Replica: r,
-			N: ceil(S_r, r) + 1,
+			N:       ceil(S_r, r) + 1,
 		},
 		Clock: make(map[string]int32),
 	}
