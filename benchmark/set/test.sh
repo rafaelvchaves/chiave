b7065afe5f239a31547f4f5708c048f21dca8152
Rafael Chaves <rafaelvchaves@gmail.com>
2022-11-29 19:35:25 -0500

    Update test script

--- benchmark/set/test.sh
+++ benchmark/set/test.sh
@@ -1,13 +1,4 @@
 TYPE=$1
+NOPS=$2
 echo "latency,throughput" >> results_${TYPE}.csv
-for ((d = 0; d < 3; d++)) do
-	for ((k = 1; k <= 9; k++)) do
-		NOPS=$((10**d * k * 1000))
-		if [[ $NOPS -gt 500000 ]]; then
-			break
-		fi
-		go run benchmark.go -mode a -nsec 5 -nops $NOPS >> results_${TYPE}.csv
-		read -n 1 -p "continue?: " 
-	done
-done
-
+go run benchmark.go -mode a -nsec 5 -nops $NOPS >> results_${TYPE}.csv
