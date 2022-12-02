d16f5e1a106b98f393b0e1f23a87d3d1c6846962
Rafael Chaves <rafaelvchaves@gmail.com>
2022-12-01 20:35:19 -0500

    Make replica String() more efficient

--- util/replica.go
+++ util/replica.go
@@ -24,7 +24,7 @@ func NewReplica(addr string, workerID int) Replica {
 }
 
 func (r Replica) String() string {
-	return r.Addr + "#" + fmt.Sprintf("%d", r.WorkerID)
+	return fmt.Sprintf("%s#%d", r.Addr, r.WorkerID)
 }
 
 type hasher struct{}
