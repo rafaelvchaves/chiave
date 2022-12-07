18f93c264d9fd6e990ca22e1d317940a57552bf5
Rafael Chaves <rafaelvchaves@gmail.com>
2022-12-06 19:39:24 -0500

    Rename output file

--- benchmark/set/test.sh
+++ benchmark/set/test.sh
@@ -1,6 +1,12 @@
 TYPE=$1
-NOPS=$2
-# echo "latency,throughput" >> results_${TYPE}.csv
-# go run benchmark.go -mode a -nsec 20 -nops $NOPS >> results_${TYPE}.csv
-go run benchmark.go -mode a -nsec 20 -nops $NOPS
-
+echo "latency,throughput" >> results_${TYPE}.csv
+for ((d = 1; d < 3; d++)) do
+	for ((k = 2; k <= 9; k++)) do
+		NOPS=$((10**d * k * 1000))
+		if [[ $NOPS -gt 500000 ]]; then
+			break
+		fi
+		go run benchmark.go -mode a -wp 100 -nops $NOPS >> results_${TYPE}.csv
+		sleep 2
+	done
+done
