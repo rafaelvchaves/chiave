bd78166098535b5a1c230527f75525bd144edff9
Rafael Chaves <rafaelvchaves@gmail.com>
2022-12-07 20:02:44 -0500

    Add w100 benchmarks

--- benchmark/set/test.sh
+++ benchmark/set/test.sh
@@ -1,12 +1,12 @@
 TYPE=$1
 echo "latency,throughput" >> results_${TYPE}.csv
-for ((d = 1; d < 3; d++)) do
-	for ((k = 3; k <= 9; k++)) do
+for ((d = 0; d < 3; d++)) do
+	for ((k = 1; k <= 9; k++)) do
 		NOPS=$((10**d * k * 1000))
-		if [[ $NOPS -gt 500000 ]]; then
+		if [[ $NOPS -gt 50000 ]]; then
 			break
 		fi
-		go run benchmark.go -mode a -wp 100 -nops $NOPS >> results_${TYPE}.csv
+		go run benchmark.go -mode a -wp 80 -nops $NOPS >> results_${TYPE}.csv
 		sleep 2
 	done
 done
