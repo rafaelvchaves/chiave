d996ae1c97f470ba5b784398fd1893857814b603
Rafael Chaves <rafaelvchaves@gmail.com>
2022-12-07 23:50:10 -0500

    Add w0,w80,w100 graphs

--- benchmark/graph.py
+++ benchmark/graph.py
@@ -6,21 +6,25 @@ from matplotlib import rc
 rc('font', **{'family':'sans-serif','sans-serif':['DejaVu Sans'],'size':10})
 rc('mathtext', **{'default':'regular'})
 
-df_op = pd.read_csv('set/results_op_w100.csv')
+graph = 'w0'
+
+plt.hlines(23, 0, 50000, linestyles='dashed', label='baseline latency', color='k')
+
+df_op = pd.read_csv(f'set/results_op_{graph}.csv')
 df_op.sort_values(by='throughput', inplace=True)
-plt.plot(df_op['throughput'], df_op['latency'] / 1000000, '--', label='op')
+plt.plot(df_op['throughput'], df_op['latency'] / 1000000, label='op')
 
-df_state = pd.read_csv('set/results_state_w100.csv')
+df_state = pd.read_csv(f'set/results_state_{graph}.csv')
 df_state.sort_values(by='throughput', inplace=True)
-plt.plot(df_state['throughput'], df_state['latency'] / 1000000, '--', label='state')
+plt.plot(df_state['throughput'], df_state['latency'] / 1000000, label='state')
 
-df_delta = pd.read_csv('set/results_delta_w100.csv')
+df_delta = pd.read_csv(f'set/results_delta_{graph}.csv')
 df_delta.sort_values(by='throughput', inplace=True)
-plt.plot(df_delta['throughput'], df_delta['latency'] / 1000000, '--', label='delta')
+plt.plot(df_delta['throughput'], df_delta['latency'] / 1000000, label='delta')
 
 plt.xlabel('throughput (operations per second)')
 plt.ylabel('95th percentile latency (milliseconds)')
 plt.title('Throughput vs. Latency')
 plt.legend()
 # plt.plot(df['throughput'], df['latency'] / 1000000)
-plt.savefig('test.png', dpi=300)
+plt.savefig(f'{graph}.png', dpi=300)
